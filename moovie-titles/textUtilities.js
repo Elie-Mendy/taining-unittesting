9248e9e39de4dfe3fe5c0ebc3d68e5b4a0a61512
Elie MENDY <emendy@efficity.com>
2023-04-27 15:38:41 +0200

    first red green refactor show case

---
+++ moovie-titles/textUtilities.js
@@ -0,0 +1,13 @@
+// var chai = require('chai');
+// var expect = chai.expect;
+
+var expect = require('chai').expect;
+
+expect(true).to.be.true;
+
+function titleCase(title) {
+    return "";
+}
+
+expect(titleCase('the great mouse detective')).to.be.a('string');
+
