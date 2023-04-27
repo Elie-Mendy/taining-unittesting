d9ca97a165eaec538862172eaa504de5a7370af5
Elie MENDY <emendy@efficity.com>
2023-04-27 15:49:18 +0200

    sample test green

--- moovie-titles/textUtilities.js
+++ moovie-titles/textUtilities.js
@@ -1,9 +1,12 @@
 var expect = require('chai').expect;
 
 function titleCase(title) {
-    return title.toUpperCase();
+    return title.split(" ").map(word => {
+        return word[0].toUpperCase() + word.substring(1);
+    }).join(' ');
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
 expect(titleCase('a')).to.be.equal('A');
+expect(titleCase('vertigo')).to.be.equal('Vertigo');
 expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
